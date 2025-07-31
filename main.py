2bc5879642621b6db1dc12f5524d609c9463e799
Kaynat Mozammil <143025590+kaynatmozammil@users.noreply.github.com>
2025-07-31 22:16:15 +0530

    Complete CRUD operations

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI ,Path ,HTTPException,Query
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel , Field , computed_field
-from typing import Annotated , Literal
+from typing import Annotated , Literal ,Optional
 import json
 app = FastAPI()
 
@@ -33,8 +33,17 @@ class Patient(BaseModel):
             return 'Normal'
         else:
             return 'Obese'
+        
+class PatientUpdate(BaseModel):
 
-def load_date():
+    name : Annotated[Optional[str] ,Field(default=None )]
+    city : Annotated[Optional[str],Field(default=None)]
+    age : Annotated[Optional[int], Field(default=None,gt=0)]
+    gender : Annotated[Optional[Literal['male', 'female','others']],Field(default=None)]
+    height : Annotated[Optional[float], Field(default=None)]
+    weight : Annotated[Optional[float], Field(default=None)]
+
+def load_data():
     with open('patients.json','r') as file:
         data = json.load(file)
 
@@ -54,14 +63,14 @@ def about():
 
 @app.get("/view")
 def view():
-    data = load_date()
+    data = load_data()
 
     return data
 
 @app.get("/patient/{patient_id}")
 def view_patient(patient_id:str = Path(...,description="ID of the patient in the DB ",example="P001")):
     # load all the patient
-    data = load_date()
+    data = load_data()
 
     if patient_id in data:
         return data[patient_id]
@@ -76,7 +85,7 @@ def sort_patient(sort_by:str = Query(..., description= "Sort on the basis of Hei
     if order_by not in ['asc','desc']:
         raise HTTPException(status_code=400, detail='Invalid order select between asc or desc ')
     
-    data=load_date()
+    data=load_data()
 
     sort_order = True if order_by =='desc' else False
     sorted_data = sorted(data.values(), key=lambda x:x.get(sort_by, 0 ),reverse=sort_order)
@@ -87,7 +96,7 @@ def sort_patient(sort_by:str = Query(..., description= "Sort on the basis of Hei
 def create_patient(patient:Patient):
 
     # load existing data
-    data = load_date()
+    data = load_data()
     # check if the patient already exist
 
     if(patient.id in data):
@@ -105,9 +114,46 @@ def create_patient(patient:Patient):
 
 
 
+@app.put('/edit/{patient_id}')
+def update_patient(patient_id:str, patient_update:PatientUpdate):
+
+    data = load_data()
+
+    if patient_id not in data:
+        raise HTTPException(status_code=404 ,detail='Patient id not found' )
+    existing_patient_info = data[patient_id]
+
+    updated_patient_info = patient_update.model_dump(exclude_unset=True)
+
+    for key, value in updated_patient_info.items():
+        existing_patient_info[key] = value 
+
+    # existing_patient_info ->  pydantic object -> updated bmi + verdict => pydanctic object -> dict
+    existing_patient_info['id'] = patient_id
+    patient_pydantic_obj =  Patient(**existing_patient_info)
+    # -> pydantic object -> dict 
+    existing_patient_info = patient_pydantic_obj.model_dump(exclude='id')
+    # add this dict to data
+    data[patient_id] = existing_patient_info
+    # save data 
+    save_data(data)
+
+    return JSONResponse(status_code=200, content={'message':'patient updated'})
 
 
+@app.delete('/delete/{patient_id}')
+def delete_patient(patient_id: str):
 
+    # load data
+    data = load_data()
+
+    if patient_id not in data:
+        raise HTTPException(status_code=404 , detail="Patient not found")
+    
+    del data[patient_id]
+
+    return JSONResponse(status_code=200 , content={'message':'patient_deleted'})
+    
 
 
 
